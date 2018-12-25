0973fb56e8abb005ef0a9939395b98c907457860
Alexey Cherevko <39003288+AlexeyCherevko1976@users.noreply.github.com>
2018-12-25 21:35:30 +0700

    Update chat.js

--- chat.js
+++ chat.js
@@ -4,7 +4,7 @@ var http = require('http').Server(app);
 
 app.use(express.static('public'));
 
-var io = require('socket.io')(http);
+//var io = require('socket.io')(http);
 
 const bodyParser = require("body-parser");
 const mongoClient = require("mongodb").MongoClient;
@@ -32,58 +32,7 @@ function delete1(){
   });  
 } 
 
-//CreateDB();
-function CreateDB(){
-    mongoClient.connect(url, function(err, client){
-          
-        const db = client.db("records");
-        const collection = db.collection("users");
-        var _id=0;
-        let records =[
-          {record: "Привет!", login: 'login1', id: _id++},
-          {record: "Как дела?", login: 'login6', id: _id++},
-          {record: "Да ничего ", login: 'login1', id: _id++},
-          {record: "Ладно", login: 'AlexeyCherevko1976', id: _id++}
-          ] ;
-        for(var i=0; i<records.length; i++){
-            collection.insertOne(records[i], function(err, result){
-              
-            if(err){ 
-                return console.log(err);
-            }
-            //console.log(result.ops);
-            client.close();
-            });            
-        }
 
-    });    
-}
-
-app.post("/createUser", jsonParser, function (req, res) {
-
-    if(!req.body) return res.sendStatus(400);      
-    var userLogin = req.body.login;
-    var userPassword = req.body.password;
-    var user = {login: userLogin, password: userPassword, n1:''};      
-    var sendInsert=calc.createUserdb(user, function (last){
-        res.send(last);
-    });
-
-}); 
-
-app.post("/IdenticUser", jsonParser, function (req, res) {
-
-    if(!req.body) return res.sendStatus(400);      
-    var userLogin = req.body.login;
-    var userPassword = req.body.password;
-    var user = {login: userLogin, password: userPassword, n1:''};      
-    var sendInsert=calc.identicUserdb(user, function (last){
-        res.send(last);
-    });
-
-}); 
- // 187 
-//app.use(express.static(__dirname + "/public"));
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/test.html');
 });
@@ -105,12 +54,6 @@ app.post("/createRecord", jsonParser, function (req, res) {
 
 });
 
-
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
